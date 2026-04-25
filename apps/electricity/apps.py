76548603aa7cdc99957be5234c642016a081c54e
1arley <arthuriarley323@gmail.com>
2026-04-24 23:58:27 -0300

    first commit

---
+++ apps/electricity/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class ElectricityConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.electricity'
---
+++ apps/electricity/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class ElectricityRecord(models.Model):
+    date = models.FloatField() 
+    day = models.CharField(max_length=10) 
+    period = models.FloatField()
+    
+    nsw_price = models.FloatField(db_column='nswprice')
+    nsw_demand = models.FloatField(db_column='nswdemand')
+    vic_price = models.FloatField(db_column='vicprice')
+    vic_demand = models.FloatField(db_column='vicdemand')
+    transfer = models.FloatField()
+    
+    demand_class = models.CharField(max_length=10, db_column='class')
+
+    class Meta:
+        db_table = 'electricity_records'
+        ordering = ['date', 'period']
+
+    def __str__(self):
+        return f"Date: {self.date} - Period: {self.period} - Class: {self.demand_class}"
