c15b89cb1724f0125134df9e9745fe9d91b20ee4
1arley <arthuriarley323@gmail.com>
2026-04-26 17:32:54 -0300

    feat: initialize axios API service with dashboard endpoint methods

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -14,8 +14,35 @@ export const api = axios.create({
     headers: { 'Content-Type': 'application/json' },
 });
 
+/* --------------------------------------------------------------
+   Endpoints Individuais (cada bloco carrega independentemente)
+   -------------------------------------------------------------- */
+
+export const getKpis = async (params = {}) => {
+    const response = await api.get('electricity/dashboard/kpis/', { params });
+    return response.data;
+};
+
+export const getDemandChart = async (params = {}) => {
+    const response = await api.get('electricity/dashboard/charts/demand/', { params });
+    return response.data;
+};
+
+export const getClassDistribution = async (params = {}) => {
+    const response = await api.get('electricity/dashboard/charts/classes/', { params });
+    return response.data;
+};
+
+export const getDayDemand = async (params = {}) => {
+    const response = await api.get('electricity/dashboard/charts/days/', { params });
+    return response.data;
+};
+
+/* --------------------------------------------------------------
+   Endpoint Agregado
+   -------------------------------------------------------------- */
+
 export const getDashboardData = async (params = {}) => {
     const response = await api.get('electricity/dashboard/', { params });
     return response.data;
 };
-
