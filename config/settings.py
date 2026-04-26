c834cc5443670f6747f0ce721ad737785adbd84e
1arley <arthuriarley323@gmail.com>
2026-04-26 16:16:51 -0300

    feat(api): explicitar permissoes padroes globais como AllowAny no drf

--- config/settings.py
+++ config/settings.py
@@ -155,6 +155,9 @@ if CORS_EXTRA:
 #  Django REST Framework
 # -----------------------------------------------------------------
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
     ],
